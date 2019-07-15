df3856b9498b94b37623ad3a28a2296a89b7a4de
Charles Cosneau <charles.cosneau@hetic.net>
2019-07-15 17:19:30 +0200

    resolve bugs on home.html

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,17 @@
 let experienceBarLevel = document.querySelector('.bar__level');
 let userExperience = 44;
 
+let accountLink = document.querySelector('.header__account');
+let accountInfo = document.getElementById('account__info');
 
+// Gestion du niveau de remplissage de la barre d'experience
 experienceBarLevel.style.width = userExperience * 0.4 + "vw";
+
+// au clic sur le nom de compte --> affichage des liens de redirection
+accountLink.addEventListener('click', function() {
+  if(accountInfo.classList.contains('account--none') == true) {
+    accountInfo.classList.replace('account--none', 'account__info');
+  } else {
+    accountInfo.classList.replace('account__info', 'account--none');
+  }
+})
